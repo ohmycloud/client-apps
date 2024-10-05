a82256cdae18ea597f0554ae8e08cfd37d981979
ohmycloud <ohmycloudy@gmail.com>
2024-10-05 10:47:08 +0800

    fix 404 page not found

--- hackernews/src/main.rs
+++ hackernews/src/main.rs
@@ -11,6 +11,8 @@ enum Route {
     Home {},
     #[route("/blog/:id")]
     Blog { id: i32 },
+    #[route("/:..route")]
+    NotFound { route: Vec<String> },
 }
 
 fn main() {
@@ -62,6 +64,17 @@ fn Home() -> Element {
                 class: "text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2",
                 onclick: move |_| count -= 1, "Down low!"
             }
+        }
+    }
+}
+
+#[component]
+fn NotFound(route: Vec<String>) -> Element {
+    let path = route.join("/");
+    rsx! {
+        div {
+            class: "flex flex-col items-center justify-center h-screen",
+            h1 { class: "text-3xl p-4", "Not Found: The page `/{path}` is not found" }
         }
     }
 }
